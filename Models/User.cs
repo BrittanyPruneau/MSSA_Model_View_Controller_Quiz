2242ebad1d36cf5806015d6a2cc3cf43e9554b33
Brittany Pruneau <69804752+BrittanyPruneau@users.noreply.github.com>
2021-08-21 16:25:22 -0600

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCQuiz.Models
+{
+    public class User
+    {
+        //   F i e l d s   &   P r o p e r t i e s
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; }
+
+        //   C o n s t r u c t o r s
+
+        //   M e t h o d s
+
+    }
+}
